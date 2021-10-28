8a1881f733e3fd444c7a115ad81f68a2f48459e2
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-10-28 15:59:30 +0200

    booking start

--- Assignment3/src/main/java/datalayer/booking/BookingStorageImpl.java
+++ Assignment3/src/main/java/datalayer/booking/BookingStorageImpl.java
@@ -3,10 +3,25 @@ package datalayer.booking;
 import dto.Booking;
 import dto.BookingCreation;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Collection;
 
 public class BookingStorageImpl implements BookingStorage {
+    private String connectionString;
+    private String username, password;
+
+    public BookingStorageImpl(String conStr, String user, String pass){
+        connectionString = conStr;
+        username = user;
+        password = pass;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(connectionString, username, password);
+    }
+    
     @Override
     public int createBooking(BookingCreation bookingCreation) throws SQLException {
         return 0;
