39ecd7438317a9f8630066af7c423d055c3c1baa
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-10-28 15:45:57 +0200

    employee

--- Assignment3/src/main/java/datalayer/employee/EmployeeStorageImpl.java
+++ Assignment3/src/main/java/datalayer/employee/EmployeeStorageImpl.java
@@ -3,23 +3,83 @@ package datalayer.employee;
 import dto.Employee;
 import dto.EmployeeCreation;
 
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class EmployeeStorageImpl implements EmployeeStorage{
+    private String connectionString;
+    private String username, password;
+
+    public EmployeeStorageImpl(String conStr, String user, String pass){
+        connectionString = conStr;
+        username = user;
+        password = pass;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(connectionString, username, password);
+    }
+
     @Override
     public Collection<Employee> getEmployeeWithId(int employeeId) throws SQLException {
-        return null;
+        var sql = "select ID, firstname, lastname from Employees where id = ?";
+        try (var con = getConnection();
+             var stmt = con.prepareStatement(sql)) {
+            stmt.setInt(1, employeeId);
+            var results = new ArrayList<Employee>();
+
+            try (var resultSet = stmt.executeQuery()) {
+                if (resultSet.next()){
+                    var id = resultSet.getInt("ID");
+                    var firstname = resultSet.getString("firstname");
+                    var lastname = resultSet.getString("lastname");
+                    Employee emp = new Employee(id, firstname, lastname);
+                    results.add(emp);
+                }
+                return results;
+            }
+        }
     }
 
     @Override
     public int createEmployee(EmployeeCreation employeeCreation) throws SQLException {
-        return 0;
+        var sql = "insert into Employees(firstname, lastname) values (?, ?)";
+        try (var con = getConnection();
+             var stmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, employeeCreation.getFirstname());
+            stmt.setString(2, employeeCreation.getLastname());
+
+            stmt.executeUpdate();
+
+            // get the newly created id
+            try (var resultSet = stmt.getGeneratedKeys()) {
+                resultSet.next();
+                int newId = resultSet.getInt(1);
+                return newId;
+            }
+        }
     }
 
     @Override
     public List<Employee> getEmployees() throws SQLException {
-        return null;
+        try (var con = getConnection();
+             var stmt = con.createStatement()) {
+            var results = new ArrayList<Employee>();
+
+            try (ResultSet resultSet = stmt.executeQuery("select ID, firstname, lastname from Employees")) {
+
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("ID");
+                    String firstname = resultSet.getString("firstname");
+                    String lastname = resultSet.getString("lastname");
+
+                    Employee emp = new Employee(id, firstname, lastname);
+                    results.add(emp);
+                }
+            }
+            return results;
+        }
     }
 }
--- Assignment3/src/main/java/dto/Employee.java
+++ Assignment3/src/main/java/dto/Employee.java
@@ -1,4 +1,34 @@
 package dto;
 
 public class Employee {
+    private final int id;
+    private final String firstname;
+    private final String lastname;
+
+    public Employee(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
 }
