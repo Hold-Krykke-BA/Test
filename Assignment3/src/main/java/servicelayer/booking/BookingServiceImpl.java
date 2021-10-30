e48a3d839e9ce0336e16d94fcba607241d807fe7
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-10-30 14:41:30 +0200

    bookingserviceimpl

--- Assignment3/src/main/java/servicelayer/booking/BookingServiceImpl.java
+++ Assignment3/src/main/java/servicelayer/booking/BookingServiceImpl.java
@@ -1,29 +1,54 @@
 package servicelayer.booking;
 
+import datalayer.booking.BookingStorage;
 import dto.Booking;
+import dto.BookingCreation;
 import dto.SmsMessage;
 
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Date;
 
 public class BookingServiceImpl implements BookingService{
+    private BookingStorage bookingStorage;
+
+    public BookingServiceImpl(BookingStorage bookingStorage) {
+        this.bookingStorage = bookingStorage;
+    }
+
     @Override
     public int createBooking(int customerId, int employeeId, Date date, String start, String end) throws BookingServiceException {
-        return 0;
+        try {
+            return bookingStorage.createBooking(new BookingCreation(customerId, employeeId, date, start, end));
+        } catch (SQLException e) {
+            throw new BookingServiceException("Booking not created");
+        }
     }
 
     @Override
     public Collection<Booking> getBookingsForCustomer(int customerId) throws BookingServiceException {
-        return null;
+        try {
+            return bookingStorage.getBookingsForCustomer(customerId);
+        } catch (SQLException e) {
+            throw new BookingServiceException("Could not return bookings for customer " + customerId);
+        }
     }
 
     @Override
     public Collection<Booking> getBookingsForEmployee(int employeeId) throws BookingServiceException {
-        return null;
+        try {
+            return bookingStorage.getBookingsForEmployee(employeeId);
+        } catch (SQLException e) {
+            throw new BookingServiceException("Could not return bookings for employee " + employeeId);
+        }
     }
 
     @Override
     public Booking getBookingById(int id) throws BookingServiceException {
-        return null;
+        try {
+            return bookingStorage.getBookingById(id);
+        } catch (SQLException e) {
+            throw new BookingServiceException("Could not return booking " + id);
+        }
     }
 }
